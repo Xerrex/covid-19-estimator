265d123218999bf872d5d07f436fa3e510411a2e
Alex kagai <alexkagai@outlook.com>
2020-04-08 09:24:01 +0300

    Finish challenge one

---
+++ src/covid19ImpactEstimator.py
@@ -0,0 +1,103 @@
+"""COVID-19 Impact Estimator
+
+
+data_provided = {
+    "region":{
+        "name": "Africa",
+        "avgAge": 19.7,
+        "avgDailyIncomeInUSD": 5,
+        "avgDailyIncomePopulation": 0.71
+    },
+    "periodType":"days",
+    "timeToElapse": 58,
+    "reportedCases": 674,
+    "population": 66622705,
+    "totalHospitalBeds": 1389614
+}
+
+estimated_values = {
+    data: {},
+    impact: {
+        "currentlyInfected": reportedCases*10,
+        "infectionsByRequestedTime": "currentlyInfected x (2 to the power of int(days/3))",
+    }, 
+    severeImpact: {
+        "currentlyInfected": reportedCases*50,
+        "infectionsByRequestedTime": "currentlyInfected x (2 to the power of int(days/3))",
+    } #your severe case esitmation
+}
+"""
+
+
+def days_calculator(periodType, period):
+    """Calculates days from period and the period type 
+    provided in arguements.
+    
+    It assumes a month has 30 days.
+
+    Arguments:
+        periodType {str:String} 
+            -- type in which period is provided ie. days, weeks, months
+
+        period {int:Integer} -- numeric value of the quantity of time.
+    
+    Returns:
+        int:Integer -- the number of days 
+    """
+
+    if periodType == "days":
+        days = period
+    elif periodType == "weeks":
+        days = period * 7
+    elif periodType == "months":
+        days = period * 30
+    else:
+        return None
+    
+    return int(days)
+
+
+def covid19ImpactEstimator(timeToElapseInDays, reportedCases, impact=None):
+    """Calculates the impact per estimates
+
+    Asssumtions:\n
+        1. At best that currently infected persons are
+            reportedCases * 10 
+        2. At Severe that currenlty infected persons are
+            reportedCases * 50
+        3. Infections double every 3 days
+
+
+    Arguments:
+
+        timeToElapseInDays {int:Integer} -- number of days for the estimate
+        reportedCases {int:Integer} -- the number of reported infected cases
+
+    Keyword Arguments:
+
+        impact {str:string} -- the impact to esitmate (default: {None})
+
+    Returns:
+
+        dict(): Dictionary -- contains the keys 'currentlyInfected'
+            'infectionsByRequestedTime' an their values
+    """
+
+
+    factor = int(timeToElapseInDays / 3)
+    multiplier = 2 ** factor
+
+    if impact == None:
+        currentlyInfected = reportedCases * 10
+        infectionsByRequestedTime = currentlyInfected * multiplier
+
+    elif impact == "severe":
+        currentlyInfected = reportedCases * 50
+        infectionsByRequestedTime = currentlyInfected * multiplier
+
+    return {
+            "currentlyInfected": currentlyInfected,
+            "infectionsByRequestedTime": infectionsByRequestedTime
+        }   
+
+
--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,24 @@
+from .covid19ImpactEstimator import days_calculator, \
+	covid19ImpactEstimator
+
+
 def estimator(data):
-  return data
+
+	# convert time to days
+	timeToElapseInDays = days_calculator(data["periodType"], 
+							data["timeToElapse"])
+
+	# impact estimate
+	impact = covid19ImpactEstimator(timeToElapseInDays, 
+				data["reportedCases"])
+
+	# 
+	severeImpact = covid19ImpactEstimator(timeToElapseInDays,
+				data["reportedCases"], impact="severe")
+
+	data = {
+		"data": data,
+		"impact": impact,
+		"severeImpact": severeImpact
+	}
+	return data
